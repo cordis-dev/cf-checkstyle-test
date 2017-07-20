4d5a9e9b0c61617fd2ceae13971bfcedd59d958c
cordis-dev <cordis.dev@gmail.com>
2017-07-20 10:42:54 +0300

    Create EmptyBlock.java

---
+++ EmptyBlock.java
@@ -0,0 +1,21 @@
+public class EmptyBlock {
+  public class test1(){
+    if(optimistic)
+      {
+      }
+      else
+      {
+      }
+  }
+  
+    public class test2(){
+    if(optimistic)
+      {
+        message = "half full";
+      }
+      else
+      {
+        message = "half empty";
+      }
+  }
+}
