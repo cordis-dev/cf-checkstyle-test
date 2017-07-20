b6b66936be30cdc507cc9a2b2c88f1ee5f951841
cordis-dev <cordis.dev@gmail.com>
2017-07-20 17:56:01 +0300

    Create EmptyStatement.java

---
+++ EmptyStatement.java
@@ -0,0 +1,16 @@
+public class EmptyStatement{
+  public void t1(){
+    doSomething();;
+    while (condition)
+    {
+        ;
+    }
+  }
+  
+  public void t1(){
+    doSomething();
+    while (condition)
+    {
+    }
+  }
+}
