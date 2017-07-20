89d7b6ffcf7d8181d19e9db87a8fa67acddb6b40
Darius <darius@adroiti.com>
2017-07-20 16:51:17 +0300

    update IllegalTokenText.java

--- IllegalTokenText.java
+++ IllegalTokenText.java
@@ -1,7 +1,7 @@
 public class IllegalTokenText {
    public void test() {
-    String t1 = "\012"; \\ bad - Unicode
-    String t1 = "\u000a"; \\ bad - octal
-    String t3 = "\t"; \\ ok
+    String t1 = "\012"; // bad - Unicode
+    String t1 = "\u000a"; // bad - octal
+    String t3 = "\t"; // ok
    }
 }
