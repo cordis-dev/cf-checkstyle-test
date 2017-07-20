844b53c2599acde53a8107f5233dd8c652c858b2
cordis-dev <cordis.dev@gmail.com>
2017-07-20 16:53:14 +0300

    Update IllegalImport.java

--- IllegalImport.java
+++ IllegalImport.java
@@ -1,2 +1,2 @@
-import sun.misc.Cleaner
-import java.lang.ref.Cleaner // standard replacement for sun.misc.Cleaner
+import sun.misc.Cleaner;
+import java.lang.ref.Cleaner; // standard replacement for sun.misc.Cleaner
